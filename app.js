739c633ec30d1604f70dde2ace3d6fe4748a9511
Josh <jromer94@gmail.com>
2015-01-13 20:45:04 -0500

    fixed for loops and added auth

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ request = require('request');
 
 function getRepos(callback) {
   var options = {
-    url: 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc',
+    url: 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc&page=3&client_id=4ed42df05d18938798f3&client_secret=e2d8403260f9a518d4b2d05b241ffe4162fd50f4',
     headers: {
       'User-Agent': 'jromer94'
     } 
@@ -16,31 +16,37 @@ function getRepos(callback) {
 }
 
 function getIssues(repo, callback) {
+  console.log('fetching issues for ' + repo.full_name);
 
   var options = {
-    url: 'https://api.github.com/repos/' + repo.full_name + '/issues',
+    url: 'https://api.github.com/repos/' + repo.full_name + '/issues?client_id=4ed42df05d18938798f3&client_secret=e2d8403260f9a518d4b2d05b241ffe4162fd50f4',
     headers: {
       'User-Agent': 'jromer94'
     } 
   }
   request(options, function(error, response, body){
-    var issues = JSON.parse(body);
-    callback(issues);
+    if(!error){
+      var issues = JSON.parse(body);
+      console.log("got " + issues.length + " issues for " + repo.full_name);
+      callback(issues);
+    } else {
+      console.log(error);
+    }
   });
 }
 
 function isIssueEasy(issue, callback){
   var labels = issue.labels;
-  for(i = 0; i < labels.length; i++) {
-    if(labels[i].name === 'bug') {
-      console.log(issue);
+  for(var i = 0; i < labels.length; i++) {
+    if(labels[i].name === 'easy') {
+      console.log(issue.url);
     }
   }
 }
 getRepos(function (repos){
-  for(i = 0; i < repos.length; i++){
+  for(var i = 0; i < repos.length; i++){
     getIssues(repos[i], function(issues){
-      for(i = 0; i < issues.length; i++) {
+      for(var i = 0; i < issues.length; i++) {
         isIssueEasy(issues[i]);
       }
     });
