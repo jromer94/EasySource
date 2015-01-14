d4a1b0ca7bdd6a44d61a066e86211096b15eaac7
Josh <jromer94@gmail.com>
2015-01-13 19:40:17 -0500

    added some initial functionatlity to search for issues with easy tag

---
+++ app.js
@@ -0,0 +1,48 @@
+request = require('request');
+
+function getRepos(callback) {
+  var options = {
+    url: 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc&page=30',
+    headers: {
+      'User-Agent': 'jromer94'
+    } 
+  }
+  request(options, function(error, response, body){
+    if(!error){
+      var items = JSON.parse(body).items;  
+      callback(items);
+    }
+  });
+}
+
+function getIssues(repo, callback) {
+
+  var options = {
+    url: 'https://api.github.com/repos/' + repo.full_name + '/issues',
+    headers: {
+      'User-Agent': 'jromer94'
+    } 
+  }
+  request(options, function(error, response, body){
+    var issues = JSON.parse(body);
+    callback(issues);
+  });
+}
+
+function isIssueEasy(issue, callback){
+  var labels = issue.labels;
+  for(i = 0; labels.length; i++) {
+    if(labels[i].name === 'easy') {
+      console.log(issue);
+    }
+  }
+}
+getRepos(function (repos){
+  for(i = 0; i < repos.length; i++){
+    getIssues(repos[i], function(issues){
+      for(i = 0; i < issues.length; i++) {
+        isIssueEasy(issues[i]);
+      }
+    });
+  }
+});
