344b27e5192c40315a22d24b6b44ae596cea7f61
Josh <jromer94@gmail.com>
2015-01-14 23:27:20 -0500

    set it up to search the first 10 pages of results

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 request = require('request');
 
-function getRepos(callback) {
+function getRepos(pageNum, callback) {
   var options = {
-    url: 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc&page=3&client_id=4ed42df05d18938798f3&client_secret=e2d8403260f9a518d4b2d05b241ffe4162fd50f4',
+    url: 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc&page='+ pageNum + '&client_id=4ed42df05d18938798f3&client_secret=e2d8403260f9a518d4b2d05b241ffe4162fd50f4',
     headers: {
       'User-Agent': 'jromer94'
     } 
@@ -43,12 +43,14 @@ function isIssueEasy(issue, callback){
     }
   }
 }
-getRepos(function (repos){
-  for(var i = 0; i < repos.length; i++){
-    getIssues(repos[i], function(issues){
-      for(var i = 0; i < issues.length; i++) {
-        isIssueEasy(issues[i]);
-      }
-    });
-  }
-});
+for(var i = 0; i < 10; i++){
+  getRepos(i, function (repos){
+    for(var i = 0; i < repos.length; i++){
+      getIssues(repos[i], function(issues){
+        for(var i = 0; i < issues.length; i++) {
+          isIssueEasy(issues[i]);
+        }
+      });
+    }
+  });
+}
