ab68524dacfb9ba801242bc6bfc269659b5dd8cd
Josh <jromer94@gmail.com>
2015-01-15 23:47:38 -0500

    added language choice to getRepo

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var request = require('request');
 var async = require('async');
 var prompt = require('prompt');
 
-function getRepos(pageNum, callback) {
+function getRepos(pageNum, language, callback) {
   var options = {
-    url: 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc&page='+ pageNum + '&client_id=4ed42df05d18938798f3&client_secret=e2d8403260f9a518d4b2d05b241ffe4162fd50f4',
+    url: 'https://api.github.com/search/repositories?q=language:'+ language + '&sort=stars&order=desc&page='+ pageNum + '&client_id=4ed42df05d18938798f3&client_secret=e2d8403260f9a518d4b2d05b241ffe4162fd50f4',
     headers: {
       'User-Agent': 'jromer94'
     } 
@@ -66,7 +66,7 @@ prompt.start();
 
 prompt.get(['language'], function (err, result) {
   async.each([1,2,3,4], function(i , callback) {
-    getRepos(i, function(repos) {
+    getRepos(i, result.language, function(repos) {
       async.each(repos, function(repo, callback){
         getIssues(repo, function(issues){
           async.each(issues, function(issue, callback){
